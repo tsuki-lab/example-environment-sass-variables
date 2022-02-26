25703d0f33b8a16df83bf71aac4d26f2afe6c4c7
hanetsuki <48175599+tsuki-lab@users.noreply.github.com>
2022-02-26 11:56:31 +0900

    Add sass includePaths config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
+const path = require('path')
+// eslint-disable-next-line @typescript-eslint/no-var-requires, no-redeclare
+const path = require('path')
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'src/styles')],
+  },
 }
 
 module.exports = nextConfig
